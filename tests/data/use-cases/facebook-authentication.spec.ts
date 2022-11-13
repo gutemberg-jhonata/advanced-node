1c0ae709451cc33f161edcac00d3c7037b7ad294
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2022-11-13 12:56:04 -0300

    ensure call LoadFacebookUserApi once

--- tests/data/use-cases/facebook-authentication.spec.ts
+++ tests/data/use-cases/facebook-authentication.spec.ts
@@ -4,10 +4,12 @@ import { AuthenticationError } from '@/domain/errors'
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
+  countCalls = 0
   result = undefined
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token
+    this.countCalls++
     return this.result
   }
 }
@@ -19,6 +21,7 @@ describe('FacebookAuthenticationUseCase', () => {
     await useCase.perform({ token: 'any_token' })
 
     expect(loadFacebookUserApi.token).toBe('any_token')
+    expect(loadFacebookUserApi.countCalls).toBe(1)
   })
 
   it('should return AuthentucationError when LoadFacebookUserApi returns undefined', async () => {
