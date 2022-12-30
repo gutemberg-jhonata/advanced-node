2c8f5bc5b27ca2e70665a6779e7d72b60718e3f3
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2022-12-29 21:10:27 -0300

    move AxiosHttpClient to is own files

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -1,14 +1,7 @@
-import { HttpGetClient } from '@/infra/http'
+import { AxiosHttpClient } from '@/infra/http'
 import axios from 'axios'
 
 jest.mock('axios')
-
-class AxiosHttpClient implements HttpGetClient {
-  async get ({ url, params }: HttpGetClient.Params): Promise<any> {
-    const result = await axios.get(url, { params })
-    return result.data
-  }
-}
 
 describe('AxiosHttpClient', () => {
   describe('get', () => {
