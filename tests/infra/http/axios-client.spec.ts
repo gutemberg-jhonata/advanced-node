d08c0ed1dd1670e23dc258c57b81ad11952b7b2d
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2022-12-29 21:04:20 -0300

    ensure AxiosHttpClient rethrow when get throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -39,6 +39,14 @@ describe('AxiosHttpClient', () => {
     })
 
     it('should return data on success', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
+
+    it('should rethrow when get throws', async () => {
       const result = await sut.get({ url, params })
 
       expect(result).toEqual('any_data')
